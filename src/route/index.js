d2bc45de9c1cd50c114b074feb2e0bf7dbd9fe29
tsurkanoleg <tsurkanoleg1993@gmail.com>
2023-09-09 21:51:00 +0300

    +

--- src/route/index.js
+++ src/route/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // Підключіть файли роутів
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
+const calc = require('./calc')
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/calc', calc)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
